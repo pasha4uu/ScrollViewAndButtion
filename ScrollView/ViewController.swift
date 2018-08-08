9165385426604a4e2bd9a1631e9b1f5bc7dbd5d2
pasha4uu <pasha7864u@gmail.com>
2018-08-08 17:35:30 +0530

    update animate Button

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginClick: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginClick.layer.cornerRadius = self.loginClick.frame.size.height/2 - 2
+        self.loginClick.layer.masksToBounds = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func loginTap(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.1, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.loginClick.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
+        }) { (_) in
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+                self.loginClick.transform = CGAffineTransform(scaleX: 1, y: 1)
+            })
+           
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
